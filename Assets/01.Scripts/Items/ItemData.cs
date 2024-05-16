a2e8691fa2b93982e8a802bddcd1dfe8212aa813
gaori217 <qwerask2725@naver.com>
2024-05-16 14:02:51 +0900

    이미지 파일 꺠짐현상으로 인해 아이콘 이미지 삭제

--- Assets/01.Scripts/Items/ItemData.cs
+++ Assets/01.Scripts/Items/ItemData.cs
@@ -8,6 +8,7 @@ public class ItemData : ScriptableObject
     public string id;
     public string displayName;
     public string description;
+    public int value;
     public Sprite icon;
     public GameObject prefab;
 }
